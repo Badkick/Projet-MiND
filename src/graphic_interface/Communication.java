02156a899d12ed2bce91a01c812653eba5a8fcc7
Mizelfindi <zaioun.hugo@yahoo.fr>
2017-07-14 17:15:31 +0200

    Fonctionnalité d'affichage d'un message d'erreur

--- src/graphic_interface/Communication.java
+++ src/graphic_interface/Communication.java
@@ -24,4 +24,16 @@ public class Communication {
 		System.exit(0);
 		//if(JOptionPane.showConfirmDialog(null,"Voulez vous vraiment quitter ? \n Attention, tout travail non enregistré sera perdu.","Fermeture",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE)==JOptionPane.YES_OPTION) System.exit(0);
 	}
+	
+	// Affiche un message d'erreur avec titre en guise de titre et message à l'intérieur.
+	public static void messageErreur(String titre, String message)
+	{
+		JOptionPane.showMessageDialog(null, message, titre, JOptionPane.ERROR_MESSAGE);
+	}
+	
+	// Affiche un message d'erreur avec "Erreur" en guise de titre et message à l'intérieur.
+	public static void messageErreur(String message)
+	{
+		messageErreur("Erreur", message);
+	}
 }
