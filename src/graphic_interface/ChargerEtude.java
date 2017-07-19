c2b56c7577bb5e8b23688bef0d1e437a035d7077
Mizelfindi <zaioun.hugo@yahoo.fr>
2017-07-19 19:18:41 +0200

    Préparation du bouton modifier
    Pour toi Maxence :p je t'ai fait une belle classe et tout !

---
+++ src/graphic_interface/ChargerEtude.java
@@ -0,0 +1,17 @@
+package graphic_interface;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChargerEtude implements ActionListener {
+
+	public ChargerEtude() {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// mettre ici ce qui se passera quand on appuiera sur le bouton Modifier
+	}
+
+}
--- src/graphic_interface/ConteneurListeEtudes.java
+++ src/graphic_interface/ConteneurListeEtudes.java
@@ -12,7 +12,7 @@ import javax.swing.ScrollPaneConstants;
 public class ConteneurListeEtudes extends ConteneurAvecImage {
 
 	// VARIABLES DE CLASSE //
-	
+
 	private Theme theme;
 	private Conteneur conteneur;
 	private Bouton bouton_back_menu;
@@ -22,9 +22,9 @@ public class ConteneurListeEtudes extends ConteneurAvecImage {
 	private Conteneur precedent;
 	private Fenetre fen;
 	private List<BoutonEtude> etudes;
-	
+
 	// CONSTRUCTEURS //
-	
+
 	public ConteneurListeEtudes(int width, int height, Theme theme, Conteneur precedent, Fenetre fen) {
 		super(width, height, theme.getBackground(), true);
 		this.theme = theme;
@@ -48,67 +48,66 @@ public class ConteneurListeEtudes extends ConteneurAvecImage {
 		this((int)dim.getWidth(),(int)dim.getHeight(),theme,precedent,fen);
 	}
 
-	
+
 	// GETTERS //
-	
+
 	public Theme getTheme()
 	{
 		return this.theme;
 	}
-	
+
 	public Conteneur getConteneur()
 	{
 		return this.conteneur;
 	}
-	
+
 	public Bouton getBoutonBack()
 	{
 		return this.bouton_back_menu;
 	}
-	
+
 	public JScrollPane getScroll()
 	{
 		return this.scroll;
 	}
-	
+
 	public Conteneur getConteneurEtudes()
 	{
 		return this.conteneur_etudes;
 	}
-	
+
 	public GroupLayout getLayout()
 	{
 		return this.layout;
 	}
-	
+
 	public Conteneur getPrecedent()
 	{
 		return this.precedent;
 	}
-	
+
 	public Fenetre getFenetre()
 	{
 		return this.fen;
 	}
-	
+
 	public List<BoutonEtude> getEtudes()
 	{
 		return this.etudes;
 	}
-	
-	
+
+
 	// METHODES //
 
 	// initialise le conteneur
 	private void initialiser()
-	{
-		
+	{		
 		this.getLayout().setAutoCreateGaps(true);
 		this.getLayout().setAutoCreateContainerGaps(true);
 		this.getScroll().setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		this.getScroll().setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		this.getScroll().setMaximumSize(new Dimension(700, 525));
-		
+
 		this.getLayout().setHorizontalGroup(
 				this.getLayout().createSequentialGroup()
 				.addGap(150)
@@ -122,7 +121,7 @@ public class ConteneurListeEtudes extends ConteneurAvecImage {
 						)
 				.addGap(150)
 				);
-		
+
 		this.getLayout().setVerticalGroup(
 				this.getLayout().createSequentialGroup()
 				.addGap(20)
@@ -131,31 +130,35 @@ public class ConteneurListeEtudes extends ConteneurAvecImage {
 				.addComponent(this.getBoutonBack())
 				.addGap(50)
 				);
-		
+
 		this.getBoutonBack().addActionListener(new ChangerConteneur(this.getFenetre(),this.getPrecedent()));
-		
+
 		this.initialiserListe();
 	}
-	
+
 	// initialise le conteneur à l'intérieur du Scroll Pane (y ajoute tous les BoutonEtude de etudes)
 	private void initialiserListe()
 	{
 		if(!this.getEtudes().isEmpty())
 		{
 			this.getConteneurEtudes().setTaille(BoutonEtude.LARGEUR, (BoutonEtude.HAUTEUR+5)*this.getEtudes().size());
-			for(BoutonEtude e : this.getEtudes()) this.getConteneurEtudes().add(e);
+			for(BoutonEtude e : this.getEtudes())
+			{
+				e.getPresenter().addActionListener(new ChargerEtude()); // MAXENCE (pour ajouter ce qui chargera l'étude) FORTEMENT RECOMMANDE : voir dans la classe chargerEtude
+				this.getConteneurEtudes().add(e);
+			}
 		}
 	}
-	
+
 	// importe les études enregistrées
 	private List<BoutonEtude> importerEtudes()
 	{
 		List<BoutonEtude> liste = new ArrayList<BoutonEtude>();
 		File file=new File("saves");
 		String[] nom_etudes=file.list();
-		
+
 		for(int i=0;i<nom_etudes.length;i++) liste.add(new BoutonEtude(this.getTheme(),nom_etudes[i]));
-		
+
 		return liste;
 	}
 }
