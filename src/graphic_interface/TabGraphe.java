9e5723a541a8d91859a5382583470a3c3cf98991
Mizelfindi <zaioun.hugo@yahoo.fr>
2017-07-22 17:09:31 +0200

    Ajout de la zone de saisie de texte
    A PEAUFINER

--- src/graphic_interface/TabGraphe.java
+++ src/graphic_interface/TabGraphe.java
@@ -1,14 +1,16 @@
 package graphic_interface;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 
 public class TabGraphe extends JTabbedPane {
-	
+
 	private static String[] EXPLICATIONS = {"<html>Remplissez les 4 cases de pourcentages pour maintenant <br> et pour après. <br> <br> Pour être valide, le total des 4 pourcentages <br> doit atteindre 100%. </html>",
 			"Salut",
 			"Je suis",
@@ -19,45 +21,81 @@ public class TabGraphe extends JTabbedPane {
 
 	// VARIABLES DE CLASSE //
 	private Theme theme;
+	
+	private int type;
 
 	private ConteneurSchema conteneur_maintenant;
 	private ConteneurSchema conteneur_apres;
 	private ConteneurSchema conteneur_risque;
 	private Conteneur conteneur_comment;
 	private Conteneur conteneur_apercu;
+	private Conteneur conteneur_risque_schema1;
+	
+	private JTextField zoneTexte;
 
 	// CONSTRUCTEUR //
 
 	public TabGraphe(int type, Theme theme) {
 		super();
+		this.type = type;
+		
 		this.conteneur_maintenant = new ConteneurSchema(type,ConteneurNouvelleEtude.TAILLE_PETIT_CONTENEUR,theme);
 		this.conteneur_apres = new ConteneurSchema(type,ConteneurNouvelleEtude.TAILLE_PETIT_CONTENEUR,theme);
-		this.conteneur_risque = new ConteneurSchema(type,ConteneurNouvelleEtude.TAILLE_PETIT_CONTENEUR,theme);
 		this.conteneur_comment = new Conteneur(ConteneurNouvelleEtude.TAILLE_PETIT_CONTENEUR);
 		this.conteneur_apercu = new Conteneur(ConteneurNouvelleEtude.TAILLE_PETIT_CONTENEUR);
+		this.conteneur_risque = new ConteneurSchema(type,ConteneurNouvelleEtude.TAILLE_PETIT_CONTENEUR,theme);
 
 		this.theme = theme;
-		
+
 		this.initialiserCommentRemplir(type);
 
-		this.add("Maintenant",this.conteneur_maintenant);
-		this.add("Après",this.conteneur_apres);
-		this.add("Risque", this.conteneur_risque);
-		this.add("Comment remplir ?",this.conteneur_comment);
-		this.add("Aperçu",this.conteneur_apercu);
+		if(type!=1)
+		{
+			this.add("Maintenant",this.conteneur_maintenant);
+			this.add("Après",this.conteneur_apres);
+			this.add("Risque", this.conteneur_risque);
+			this.add("Comment remplir ?",this.conteneur_comment);
+			this.add("Aperçu",this.conteneur_apercu);
+			
+			this.setTabComponentAt(2, new CustomTab("Risque",theme));
+			this.getTabComponentAt(2).setForeground(Color.red);
+			this.setTabComponentAt(3, new CustomTab("Comment remplir ?",theme));
+			this.setTabComponentAt(4, new CustomTab("Aperçu",theme));
 
+			this.conteneur_risque.addComponentListener(new Verification100PourCent(type, this.conteneur_risque, (CustomTab) this.getTabComponentAt(2)));
+		}
+		else
+		{
+			this.conteneur_risque_schema1 = new Conteneur(ConteneurNouvelleEtude.TAILLE_PETIT_CONTENEUR);
+			this.add("Maintenant",this.conteneur_maintenant);
+			this.add("Après",this.conteneur_apres);
+			this.add("Encore après", this.conteneur_risque);
+			this.add("Risque", this.conteneur_risque_schema1);
+			this.add("Comment remplir ?",this.conteneur_comment);
+			this.add("Aperçu",this.conteneur_apercu);
+			
+			this.setTabComponentAt(2, new CustomTab("Encore après",theme));
+			this.getTabComponentAt(2).setForeground(Color.red);
+			this.setTabComponentAt(3, new CustomTab("Risque",theme));
+			this.setTabComponentAt(4, new CustomTab("Comment remplir ?",theme));
+			this.setTabComponentAt(5, new CustomTab("Aperçu",theme));
+			
+			zoneTexte = new JTextField();
+			Dimension tailleZoneTexte = new Dimension(300,300);
+			zoneTexte.setPreferredSize(tailleZoneTexte);
+			zoneTexte.setMinimumSize(tailleZoneTexte);
+			zoneTexte.setMaximumSize(tailleZoneTexte);
+			zoneTexte.setFont(this.getTheme().getPolice());
+			this.conteneur_risque_schema1.add(zoneTexte);
+		}
+		
 		this.setTabComponentAt(0, new CustomTab("Maintenant",theme));
 		this.getTabComponentAt(0).setForeground(Color.red);
 		this.setTabComponentAt(1, new CustomTab("Après",theme));
 		this.getTabComponentAt(1).setForeground(Color.red);
-		this.setTabComponentAt(2, new CustomTab("Risque",theme));
-		this.getTabComponentAt(2).setForeground(Color.red);
-		this.setTabComponentAt(3, new CustomTab("Comment remplir ?",theme));
-		this.setTabComponentAt(4, new CustomTab("Aperçu",theme));
 		
 		this.conteneur_maintenant.addComponentListener(new Verification100PourCent(type, this.conteneur_maintenant, (CustomTab) this.getTabComponentAt(0)));
 		this.conteneur_apres.addComponentListener(new Verification100PourCent(type, this.conteneur_apres, (CustomTab) this.getTabComponentAt(1)));
-		this.conteneur_risque.addComponentListener(new Verification100PourCent(type, this.conteneur_risque, (CustomTab) this.getTabComponentAt(2)));
 	}
 
 	// GETTERS //
@@ -75,9 +113,13 @@ public class TabGraphe extends JTabbedPane {
 		return conteneur_apres;
 	}
 	
-	public ConteneurSchema getConteneur_risque()
+	public ConteneurSchema getConteneur_encore_apres() {
+		return (this.type!=1) ? null : this.conteneur_risque;
+	}
+
+	public Conteneur getConteneur_risque()
 	{
-		return this.conteneur_risque;
+		return (this.type!=1) ? this.conteneur_risque : this.conteneur_risque_schema1;
 	}
 
 	public Conteneur getConteneur_comment() {
@@ -86,6 +128,10 @@ public class TabGraphe extends JTabbedPane {
 
 	public Conteneur getConteneur_apercu() {
 		return conteneur_apercu;
+	}
+	
+	public JTextField getZoneTexte() {
+		return zoneTexte;
 	}
 
 	// SETTERS //
