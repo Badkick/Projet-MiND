b7a70d87aa8ad660b55034cff0138eb566f9036d
Mizelfindi <zaioun.hugo@yahoo.fr>
2017-07-24 00:38:03 +0200

    Réglage du bug
    C'était bien un mauvais get ;p

--- src/graphic_interface/UpdateEtude.java
+++ src/graphic_interface/UpdateEtude.java
@@ -49,7 +49,7 @@ public class UpdateEtude implements ComponentListener {
 			try {FenetreAccueil.etude.getS1().setApr_etabl_prot(tab.getConteneur_apres().getEtabl_prot());} catch(NullPointerException ex) {}
 			try {FenetreAccueil.etude.getS1().setApr_mena_mar(tab.getConteneur_apres().getMena_mar());} catch(NullPointerException ex) {}
 			try {FenetreAccueil.etude.getS1().setApr_press_envir(tab.getConteneur_apres().getPress_envir());} catch(NullPointerException ex) {}
-			try {FenetreAccueil.etude.getS1().setRis_contr_tech(((ConteneurSchema) tab.getConteneur_risque()).getContr_tech());} catch(NullPointerException ex) {}
+			try {FenetreAccueil.etude.getS1().setRis_contr_tech(((ConteneurSchema) tab.getConteneur_encore_apres()).getContr_tech());} catch(NullPointerException ex) {}
 			try {FenetreAccueil.etude.getS1().setRis_etabl_prot(((ConteneurSchema) tab.getConteneur_risque()).getEtabl_prot());} catch(NullPointerException ex) {}
 			try {FenetreAccueil.etude.getS1().setRis_mena_mar(((ConteneurSchema) tab.getConteneur_risque()).getMena_mar());} catch(NullPointerException ex) {}
 			try {FenetreAccueil.etude.getS1().setRis_press_envir(((ConteneurSchema) tab.getConteneur_risque()).getPress_envir());} catch(NullPointerException ex) {}
