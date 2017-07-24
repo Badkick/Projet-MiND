970b0a2c0e000761fbb676321785a5e259eb4d44
Gafpix <max.monfort@hotmail.fr>
2017-07-24 19:39:30 +0200

    Finition chargement pour tous les schémas

--- src/graphic_interface/ChargerEtude.java
+++ src/graphic_interface/ChargerEtude.java
@@ -20,19 +20,19 @@ public class ChargerEtude implements ActionListener {
 		// mettre ici ce qui se passera quand on appuiera sur le bouton Modifier
 		String contraintes="saves"+File.separator+nom+File.separator+"Contraintes.txt";
 		Schema1 schema1=null;
-		Pourcentage p_1=null;
-		Pourcentage p_2=null;
-		Pourcentage r_1=null;
-		Pourcentage p_3=null;
-		Pourcentage p_4=null;
-		Pourcentage r_2=null;
-		Pourcentage p_5=null;
-		Pourcentage p_6=null;
-		Pourcentage r_3=null;
-		Pourcentage p_7=null;
-		Pourcentage p_8=null;
-		Pourcentage r_4=null;
 		try {
+			Pourcentage p_1=null;
+			Pourcentage p_2=null;
+			Pourcentage r_1=null;
+			Pourcentage p_3=null;
+			Pourcentage p_4=null;
+			Pourcentage r_2=null;
+			Pourcentage p_5=null;
+			Pourcentage p_6=null;
+			Pourcentage r_3=null;
+			Pourcentage p_7=null;
+			Pourcentage p_8=null;
+			Pourcentage r_4=null;
 			BufferedReader aLire_1 = new BufferedReader(new FileReader(contraintes));
 			try{
 				p_1=new Pourcentage(Integer.valueOf(aLire_1.readLine()));
@@ -80,11 +80,12 @@ public class ChargerEtude implements ActionListener {
 		
 		String structures="saves"+File.separator+nom+File.separator+"Structures.txt";
 		Schema2 schema2=null;
-		Structure st_1=null;
-		Structure st_2=null;
-		Structure st_ris=null;
+		
 		try {
 			BufferedReader aLire_2 = new BufferedReader(new FileReader(structures));
+			Structure st_1=null;
+			Structure st_2=null;
+			Structure st_ris=null;
 			try{
 				st_1=Structure.getByName(aLire_2.readLine());
 			}catch(Exception ex){}
@@ -205,16 +206,135 @@ public class ChargerEtude implements ActionListener {
 		Schema4 schema4=null;
 		try {
 			BufferedReader aLire_4 = new BufferedReader(new FileReader(relations));
-			Consensus cons_1=new Consensus(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Consensus cons_2=new Consensus(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Consensus ris_1=new Consensus(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Negociation nego_1=new Negociation(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Negociation nego_2=new Negociation(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Negociation ris_2=new Negociation(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Opposition opp_1=new Opposition(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Opposition opp_2=new Opposition(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			Opposition ris_3=new Opposition(new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())),new Pourcentage(Integer.valueOf(aLire_4.readLine())));
-			
+			Pourcentage p_1=null;
+			Pourcentage p_2=null;
+			Pourcentage p_3=null;
+			Pourcentage p_4=null;
+			Pourcentage p_5=null;
+			Pourcentage p_6=null;
+			Pourcentage p_7=null;
+			Pourcentage p_8=null;
+			Pourcentage p_9=null;
+			Pourcentage p_10=null;
+			Pourcentage p_11=null;
+			Pourcentage p_12=null;
+			Pourcentage p_13=null;
+			Pourcentage p_14=null;
+			Pourcentage p_15=null;
+			Pourcentage p_16=null;
+			Pourcentage p_17=null;
+			Pourcentage p_18=null;
+			Pourcentage p_19=null;
+			Pourcentage p_20=null;
+			Pourcentage p_21=null;
+			Pourcentage p_22=null;
+			Pourcentage p_23=null;
+			Pourcentage p_24=null;
+			Pourcentage p_25=null;
+			Pourcentage p_26=null;
+			Pourcentage p_27=null;
+			Pourcentage p_28=null;
+			Pourcentage p_29=null;
+			Pourcentage p_30=null;
+			try{
+				p_1=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_2=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_3=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Consensus cons_1=new Consensus(p_1,p_2,p_3);
+			try{	
+				p_4=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_5=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_6=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Consensus cons_2=new Consensus(p_4,p_5,p_6);
+			try{
+				p_7=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_8=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_9=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Consensus ris_1=new Consensus(p_7,p_8,p_9);
+			try{
+				p_10=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_11=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_12=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Negociation nego_1=new Negociation(p_10,p_11,p_12);
+			try{
+				p_13=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_14=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_15=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Negociation nego_2=new Negociation(p_13,p_14,p_15);
+			try{
+				p_16=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_17=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_18=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Negociation ris_2=new Negociation(p_16,p_17,p_18);
+			try{
+				p_19=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_20=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_21=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_22=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Opposition opp_1=new Opposition(p_19,p_20,p_21,p_22);
+			try{
+				p_23=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_24=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_25=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_26=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Opposition opp_2=new Opposition(p_23,p_24,p_25,p_26);
+			try{
+				p_27=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_28=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_29=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_30=new Pourcentage(Integer.valueOf(aLire_4.readLine()));
+			}catch(Exception ex){}
+			Opposition ris_3=new Opposition(p_27,p_28,p_29,p_30);
 			aLire_4.close();
 			schema4=new Schema4(cons_1,cons_2,ris_1, nego_1,nego_2,ris_2, opp_1,opp_2, ris_3);
 			
@@ -227,24 +347,78 @@ public class ChargerEtude implements ActionListener {
 		Schema5 schema5=null;
 		try {
 			BufferedReader aLire_5 = new BufferedReader(new FileReader(identites));
-			/*Pourcentage p_1=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_2=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_3=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_4=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_5=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_6=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_7=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_8=new Pourcentage(Integer.valueOf(aLire_5.readLine()));*/
-			Pourcentage p_9=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_10=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_11=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_12=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_13=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_14=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_15=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_16=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_17=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
-			Pourcentage p_18=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			Pourcentage p_1=null;
+			Pourcentage p_2=null;
+			Pourcentage p_3=null;
+			Pourcentage p_4=null;
+			Pourcentage p_5=null;
+			Pourcentage p_6=null;
+			Pourcentage p_7=null;
+			Pourcentage p_8=null;
+			Pourcentage p_9=null;
+			Pourcentage p_10=null;
+			Pourcentage p_11=null;
+			Pourcentage p_12=null;
+			Pourcentage p_13=null;
+			Pourcentage p_14=null;
+			Pourcentage p_15=null;
+			Pourcentage p_16=null;
+			Pourcentage p_17=null;
+			Pourcentage p_18=null;
+			try{
+				p_1=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_2=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_3=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{	
+				p_4=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_5=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_6=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_7=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_8=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_9=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_10=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_11=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_12=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_13=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_14=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_15=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_16=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_17=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
+			try{
+				p_18=new Pourcentage(Integer.valueOf(aLire_5.readLine()));
+			}catch(Exception ex){}
 			
 			aLire_5.close();
 			schema5=new Schema5(p_1,p_2,p_3,p_4,p_5,p_6,p_7,p_8,p_9,p_10,p_11,p_12, p_13, p_14, p_15, p_16, p_17, p_18);
@@ -256,11 +430,20 @@ public class ChargerEtude implements ActionListener {
 		
 		String mondes="saves"+File.separator+nom+File.separator+"Mondes.txt";
 		Schema6 schema6=null;
+		Entreprise e_1=null;
+		Entreprise e_2=null;
+		Entreprise e_3=null;
 		try {
 			BufferedReader aLire_6 = new BufferedReader(new FileReader(mondes));
-			Entreprise e_1=Entreprise.getByName(aLire_6.readLine());
-			Entreprise e_2=Entreprise.getByName(aLire_6.readLine());
-			Entreprise e_3=Entreprise.getByName(aLire_6.readLine());
+			try{
+				e_1=Entreprise.getByName(aLire_6.readLine());
+			}catch(NullPointerException ex){}
+			try{
+				e_2=Entreprise.getByName(aLire_6.readLine());
+			}catch(NullPointerException ex){}
+			try{
+				e_3=Entreprise.getByName(aLire_6.readLine());
+			}catch(NullPointerException ex){}
 			
 			aLire_6.close();
 			schema6=new Schema6(e_1,e_2,e_3);
